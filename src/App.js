5ff881143262ba2ad93deb70bd09657ab8d86819
bhargavi.palineni <bhargavi_palineni@comcast.com>
2021-09-30 11:43:16 +0000

    Adding standard licensing files & Headers on Required files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+/*
+* If not stated otherwise in this file or this component's LICENSE file the
+* following copyright and licenses apply:
+*
+* Copyright 2020 Metrological
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 import {Router, Utils} from "@lightningjs/sdk";
 import {Automotive} from "@lightningjs/automotive";
 import routes from "./lib/routes";
@@ -7,7 +25,7 @@ import {DemoSelector} from "./widgets";
 export default class App extends Router.App {
     static getFonts() {
         return [
-            {family: 'julius', url: Utils.asset('fonts/JuliusSansOne-Regular.ttf'), descriptors: {}},
+            {family: 'julius', url: Utils.asset('fonts/JuliusSansOne/JuliusSansOne-Regular.ttf'), descriptors: {}},
         ];
     }
 
@@ -90,4 +108,3 @@ export default class App extends Router.App {
         return states;
     }
 }
-
